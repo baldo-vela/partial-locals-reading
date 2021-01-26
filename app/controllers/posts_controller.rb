ca2eb3ff6a1a23cccdc55ac68d8edd770c913785
baldo-vela <baldo.vela+github@gmail.com>
2021-01-26 16:28:27 -0600

    commit tree fad5796dbeb37a3432c33d9487c4ad32c85935a5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
-    @author = @post.author
+    # '@author = @post.author' replaced with 'render local'
   end
 
   def index
